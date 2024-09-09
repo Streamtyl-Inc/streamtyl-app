4f842f3eb6869b3e105b1683051579a42f9e01de
dnor-dev <anorue4rss@hotmail.com>
2024-09-09 23:44:46 +0100

    chore: livepeer client key

--- utils/livepeer-client.ts
+++ utils/livepeer-client.ts
@@ -3,12 +3,10 @@ import { createReactClient, studioProvider } from "@livepeer/react";
 
 export const livepeerClient = createReactClient({
   provider: studioProvider({
-    apiKey: process.env.NEXT_PUBLIC_LIVEPEER_API_KEY!,
-   
+    apiKey: "b6aad7e2-f20b-4550-a209-59e9da8972fd",
   }),
 });
 
 export const livepeerService = new Livepeer({
-  apiKey: process.env.NEXT_PUBLIC_LIVEPEER_API_KEY!,
-  
+  apiKey: "b6aad7e2-f20b-4550-a209-59e9da8972fd",
 });
