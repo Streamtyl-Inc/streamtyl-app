b4aa882fda65cbe4683045ba776dd06102ed2060
scapula07 <bartholomewonogwu@yahoo.com>
2024-09-09 12:01:31 +0100

    greyed out xmtp

--- utils/livepeer-client.ts
+++ utils/livepeer-client.ts
@@ -1,14 +1,14 @@
 import { Livepeer } from "livepeer";
 import { createReactClient, studioProvider } from "@livepeer/react";
-var apiKey="bce412b1-9641-4983-8157-59d37c6b9dcd"
+
 export const livepeerClient = createReactClient({
   provider: studioProvider({
-    // apiKey: process.env.NEXT_PUBLIC_LIVEPEER_API_KEY!,
-    apiKey:"bce412b1-9641-4983-8157-59d37c6b9dcd"
+    apiKey: process.env.NEXT_PUBLIC_LIVEPEER_API_KEY!,
+  
   }),
 });
 
 export const livepeerService = new Livepeer({
-  apiKey:"bce412b1-9641-4983-8157-59d37c6b9dcd"
+   apiKey: process.env.NEXT_PUBLIC_LIVEPEER_API_KEY!,
   
 });
